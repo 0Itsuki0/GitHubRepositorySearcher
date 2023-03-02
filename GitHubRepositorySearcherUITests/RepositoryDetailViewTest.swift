5c46274b0b6ed6e908ce7bc66099dd7a9555564b
0Itsuki0 <2018.itsuki@gmail.com>
2023-03-02 18:39:20 +0900

    Add UI Tests.

---
+++ GitHubRepositorySearcherUITests/RepositoryDetailViewTest.swift
@@ -0,0 +1,73 @@
+//
+//  RepositoryDetailViewTest.swift
+//  GitHubRepositorySearcherUITests
+//
+//  Created by イツキ on 2023/03/02.
+//
+
+import XCTest
+
+final class RepositoryDetailViewTest: XCTestCase {
+
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        
+        
+    }
+    
+    override func tearDownWithError() throws {
+        app.terminate()
+        app = nil
+        try super.tearDownWithError()
+    }
+    
+    
+    func testRepositoryDetailView() {
+        
+        let searchBar = app.otherElements["SearchBar"]
+        searchBar.tap()
+        searchBar.typeText("swift")
+        
+        let sendButton = app.buttons["SendButton"]
+        sendButton.tap()
+        
+        let repositoryListTableView = app.tables["RepositoryListTableView"]
+        let repositoryCells = repositoryListTableView.cells
+        XCTAssertTrue(repositoryCells.firstMatch.waitForExistence(timeout: 10))
+
+        let repositoryCell = repositoryCells.element(boundBy: 0)
+        repositoryCell.tap()
+        
+        let repositoryDetailAvatarImageView = app.images["RepositoryDetailAvatarImageView"]
+        XCTAssertTrue(repositoryDetailAvatarImageView.exists, "The avatar image view exists.")
+        
+        let repositoryDetailNameLabel = app.staticTexts["RepositoryDetailNameLabel"]
+        XCTAssertTrue(repositoryDetailNameLabel.exists, "The repository name label exists.")
+        
+        let repositoryDetailDescriptionLabel = app.staticTexts["RepositoryDetailDescriptionLabel"]
+        XCTAssertTrue(repositoryDetailDescriptionLabel.exists, "The repository description label exists.")
+        
+        let repositoryDetailLanguageLabel = app.staticTexts["RepositoryDetailLanguageLabel"]
+        XCTAssertTrue(repositoryDetailLanguageLabel.exists, "The repository language label exists.")
+        
+        let repositoryDetailOpenIssuesCountLabel = app.staticTexts["RepositoryDetailOpenIssuesCountLabel"]
+        XCTAssertTrue(repositoryDetailOpenIssuesCountLabel.exists, "The open issues count label exists.")
+        
+        let repositoryDetailWatchersCountLabel = app.staticTexts["RepositoryDetailWatchersCountLabel"]
+        XCTAssertTrue(repositoryDetailWatchersCountLabel.exists, "The watchers count label exists.")
+        
+        let repositoryDetailStarCountLabel = app.staticTexts["RepositoryDetailStarCountLabel"]
+        XCTAssertTrue(repositoryDetailStarCountLabel.exists, "The star count label exists.")
+        
+        let repositoryDetailForksCountLabel = app.staticTexts["RepositoryDetailForksCountLabel"]
+        XCTAssertTrue(repositoryDetailForksCountLabel.exists, "The forks count label exists.")
+        
+    }
+
+    
+}
--- GitHubRepositorySearcherUITests/TableViewTest.swift
+++ GitHubRepositorySearcherUITests/TableViewTest.swift
@@ -6,8 +6,10 @@
 //
 
 import XCTest
+import GitHubRepositorySearcher
 
 final class TableViewTest: XCTestCase {
+
     var app: XCUIApplication!
     
     override func setUpWithError() throws {
@@ -15,6 +17,7 @@ final class TableViewTest: XCTestCase {
         continueAfterFailure = false
         app = XCUIApplication()
         app.launch()
+        
         
     }
     
@@ -25,22 +28,57 @@ final class TableViewTest: XCTestCase {
     }
     
     
-    
     func testTableViewInteraction() {
+        
+        let searchBar = app.otherElements["SearchBar"]
+        searchBar.tap()
+        searchBar.typeText("swift")
+        
+        let sendButton = app.buttons["SendButton"]
+        sendButton.tap()
+        
         let repositoryListTableView = app.tables["RepositoryListTableView"]
-        XCTAssertTrue(repositoryListTableView.exists, "The repository list tableview exists")
+        XCTAssertTrue(repositoryListTableView.exists, "The repository list tableview exists.")
         let repositoryCells = repositoryListTableView.cells
+        XCTAssertTrue(repositoryCells.firstMatch.waitForExistence(timeout: 10))
+        
+
+        
+        if repositoryCells.count > 0 {
+            let count: Int = (repositoryCells.count - 1)
+         
+            let promise = expectation(description: "Wait for table cells")
+         
+            for i in stride(from: 0, to: count , by: 1) {
+                // Grab the first cell and verify that it exists and tap it
+                let repositoryCell = repositoryCells.element(boundBy: i)
+                XCTAssertTrue(repositoryCell.exists, "The \(i) cell is in place on the table")
+                
+                let repositoryCellNameLabel = repositoryCell.staticTexts["RepositoryCellNameLabel"]
+                XCTAssertTrue(repositoryCellNameLabel.exists, "The repository cell name label exists.")
+                
+                let repositoryCellDescriptionLabel = repositoryCell.staticTexts["RepositoryCellDescriptionLabel"]
+                XCTAssertTrue(repositoryCellDescriptionLabel.exists, "The repository cell description label exists.")
+                
+                let repositoryCellLanguageLabel = repositoryCell.staticTexts["RepositoryCellLanguageLabel"]
+                XCTAssertTrue(repositoryCellLanguageLabel.exists, "The repository cell language label exists.")
+                
+                repositoryCell.tap()
+         
+                if i == (count - 1) {
+                    promise.fulfill()
+                }
+                // Back
+                app.navigationBars.buttons.element(boundBy: 0).tap()
+            }
+            waitForExpectations(timeout: 20, handler: nil)
+            XCTAssertTrue(true, "Finished validating the table cells")
+         
+        } else {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
         
 
     }
-    
 
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
 }
